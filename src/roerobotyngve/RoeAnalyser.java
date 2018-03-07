56e90d1f05ad29928661dca25d4f73f7d92ee001
Yngve <Yngve@158.38.92.130>
2018-03-07 10:27:46 +0100

    SuperDuper Commit

--- src/roerobotyngve/RoeAnalyser.java
+++ src/roerobotyngve/RoeAnalyser.java
@@ -9,34 +9,102 @@ package roerobotyngve;
  *
  * @author Yngve
  */
-public class RoeAnalyser {
+public class RoeAnalyser implements Runnable {
 
-     //State enum for the switchcase
-    private enum State
-    {
+    @Override
+    public void run() {
+
+    }
+
+    //State enum for the switchcase
+    private enum State {
         Calibrate,
         Running,
-        Waiting;
+        Stop,
+        Waiting,
+        Pause;
     }
 
-    
-    
     private RoeAnalyserDevice roeAnalyser;
-    //State enum
+    // State enum
     private State state;
-    
-    //Flag to remember if the tray is open or not
+
+    // Flag to remember if the tray is open or not
     private boolean trayOpen;
-   //The amount of pictures to be taken
+    // The amount of pictures to be taken
     private int nrOfPicturesToBeTaken;
+    // The tray number currently opperated.   
     private int currentTray;
-    
-    public void RoeAnalyser() {
+
+    // Time for each cycle
+    private long timePerCycle;
+
+    /**
+     * Constructor
+     */
+    public RoeAnalyser() {
         this.roeAnalyser = new RoeAnalyserDevice();
-        trayOpen = false;
-        currentTray = 1;
+        this.trayOpen = false;
+        this.currentTray = 1;
+        this.timePerCycle = 0;
     }
 
+    /**
+     * Set the time per cycle for the robot The time per cycle is the time for
+     * the robot to wait before it starts over
+     *
+     * @param time given in minutes.
+     */
+    public void setTimePerCycle(int time) {
+        this.timePerCycle = (time / 60) * 10 ^ -3;
+    }
+
+    
+    /**
+     * Get time per cycle in millisecounds. 
+     * @return time per cycle in millisecounds. 
+     */
+    public long getTimePerCycle() {
+        return this.timePerCycle;
+    }
+
+    /**
+     * Do calibrate the robot.
+     */
+    public void doCalibrate() {
+        state = State.Calibrate;
+        this.cycleCase(state);
+    }
+
+    /**
+     * Do Start the robot sycle
+     */
+    public void doStartRobot() {
+        state = State.Running;
+        this.cycleCase(state);
+    }
+
+    /**
+     * Do pause the robot. Prevent the robot from doing enything.
+     */
+    public void doPauseRobot() {
+        state = State.Pause;
+        this.cycleCase(state);
+    }
+
+    /**
+     * Stopps the robot
+     */
+    public void doStopRobot() {
+        state = State.Stop;
+        this.cycleCase(state);
+    }
+
+    /**
+     * Cycle switch case for controlling the robot.
+     *
+     * @param state
+     */
     private void cycleCase(State state) {
         switch (state) {
             // Sends calibatrion cmd. 
@@ -46,20 +114,21 @@ public class RoeAnalyser {
                 this.roeAnalyser.calibrate();
             // Starts the calibration cycle
 
+            // Run the picking operation. 
             case Running:
                 //Find number of trays in the rack. 
                 int nrOfTrays = roeAnalyser.getNumberOfTrays();
                 // For each number in the rack run a roe removal sequense. {
-                  
+
                 for (int i = currentTray; i > nrOfTrays; i++) {
                     this.currentTray = i;
                     // Open one tray
-                    this.roeAnalyser.openTray(i);  
+                    this.roeAnalyser.openTray(i);
                     this.trayOpen = true;
                     // Find the number of pictures needed to be taken for covering all coordinates in a tray. 
                     nrOfPicturesToBeTaken = 1;
                     //For each picture needed to be taken (Frames) ...
-                    for (int j = 1; j > nrOfPicturesToBeTaken; i++){                     
+                    for (int j = 1; j > nrOfPicturesToBeTaken; i++) {
                         //  Take a picture. 
                         this.roeAnalyser.takePicture(j);
                         //      Find dead roe in picture. 
@@ -71,22 +140,13 @@ public class RoeAnalyser {
                     this.roeAnalyser.closeTray(i);
                     this.trayOpen = false;
                 }
-                
-                currentTray = 1;
                 break;
+
+            // Wait for timer to expire. 
             case Waiting:
                 //Wait for interval
                 break;
-        }   
-    }
-    
-    
-    /**
-     * Start the robot
-     */
-    public void startRobot()
-    {
-        state = State.Running;
+        }
     }
 
 }
